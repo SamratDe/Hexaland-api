7c2b974941cd75891363bcfe9fa18b6332592c63
Samrat De <rajmalhotra2016iitjee@gmail.com>
2020-06-12 09:10:51 +0530

    made the server and routes

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const hexalandRoute = require('./routes/api/hexaland')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use('/api/hexaland', hexalandRoute)
+
+app.listen(PORT, () => {
+	console.log(`Server up and running on PORT ${PORT}`)
+})
